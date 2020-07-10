c0a5ff1aaa54d7bd6711a26c329726f1e747561f
lilpolymath <ayofavour7@gmail.com>
2020-07-10 21:27:17 +0100

    feat: setting up routing.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import './App.css';
 
 // import HeroOne from './screens/Hero1';
@@ -7,9 +9,37 @@ import HeroThree from './screens/Hero3';
 
 function App() {
   return (
-    <>
+    <div>
       <HeroThree />
-    </>
+    </div>
+    // <Router>
+    //   <div>
+    //     <ul>
+    //       <li>
+    //         <Link to='/one'>One</Link>
+    //       </li>
+    //       <li>
+    //         <Link to='/two'>Two</Link>
+    //       </li>
+    //       <li>
+    //         <Link to='/three'>Three</Link>
+    //       </li>
+    //     </ul>
+
+    //     <hr />
+    //     <Switch>
+    //       <Route exact path='/one'>
+    //         <HeroOne />
+    //       </Route>
+    //       <Route path='/two'>
+    //         <HeroTwo />
+    //       </Route>
+    //       <Route path='/three'>
+    //         <HeroThree />
+    //       </Route>
+    //     </Switch>
+    //   </div>
+    // </Router>
   );
 }
 
