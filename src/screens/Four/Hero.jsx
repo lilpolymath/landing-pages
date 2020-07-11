8cb1d361a98a5abd0306e09ee067d6fa0008e017
lilpolymath <ayofavour7@gmail.com>
2020-07-11 23:10:04 +0100

    feat: image transition

--- src/screens/Four/Hero.jsx
+++ src/screens/Four/Hero.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { animated, useSprings } from 'react-spring';
 
 import Google from '../../assets/icons/Google';
 import LinkedIn from '../../assets/icons/LinkedIn';
 import Twitter from '../../assets/icons/Twitter';
 
-const imageSrc =
-  'https://res.cloudinary.com/favourcodes/image/upload/v1594425842/Placeholder_2_fl0psx.png';
-
-const nextImage =
-  'https://res.cloudinary.com/favourcodes/image/upload/v1594493520/Placeholder_3_ythadq.png';
-
 const Hero = () => {
+  const [index, setIndex] = useState(0);
+
+  const images = [
+    {
+      index: 1,
+      src:
+        'https://res.cloudinary.com/favourcodes/image/upload/v1594228052/Placeholder_1_ye5ru5.png',
+    },
+    {
+      index: 2,
+      src:
+        'https://res.cloudinary.com/favourcodes/image/upload/v1594425842/Placeholder_2_fl0psx.png',
+    },
+  ];
+
   return (
     <main>
       <div className='hero'>
@@ -30,17 +40,29 @@ const Hero = () => {
           </div>
         </div>
         <div className='index'>
-          <div className='numbers active'>1</div>
-          <div className='numbers'>2</div>
-          <div className='numbers'>3</div>
+          {images.map(item => (
+            <animated.div
+              onClick={() => setIndex(item.index - 1)}
+              className={
+                index === item.index - 1 ? 'numbers active' : 'numbers'
+              }
+            >
+              {item.index}
+            </animated.div>
+          ))}
         </div>
       </div>
+
       <div className='hero-image'>
         <figure>
-          <img className='image' alt='' src={imageSrc} />
+          <img className='image' alt='' src={images[index].src} />
         </figure>
         <div className='next'>
-          <img className='next-image' alt='' src={nextImage} />
+          <img
+            className='next-image'
+            alt=''
+            src={images[(index + 1) % images.length].src}
+          />
         </div>
       </div>
     </main>
