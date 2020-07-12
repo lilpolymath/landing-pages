ce85b7dfe0e9494f25ab4ba137a9c36ed5dee71d
lilpolymath <ayofavour7@gmail.com>
2020-07-12 11:45:16 +0100

    feat: animated image entry.

--- src/screens/Four/Hero.jsx
+++ src/screens/Four/Hero.jsx
@@ -1,41 +1,59 @@
-import React, { useState } from 'react';
-import { animated, useSprings } from 'react-spring';
+import React, { useState, useCallback } from 'react';
+import { animated, useTransition, config } from 'react-spring';
 
 import Google from '../../assets/icons/Google';
 import LinkedIn from '../../assets/icons/LinkedIn';
 import Twitter from '../../assets/icons/Twitter';
 
 const Hero = () => {
-  const [index, setIndex] = useState(0);
+  const [[index, next], setIndex] = useState([0, 1]);
   const [active, setActive] = useState(false);
 
   const images = [
     {
       index: 1,
-      src:
-        'https://res.cloudinary.com/favourcodes/image/upload/v1594228052/Placeholder_1_ye5ru5.png',
+      src: 'v1594228052/Placeholder_1_ye5ru5.png',
     },
     {
       index: 2,
-      src:
-        'https://res.cloudinary.com/favourcodes/image/upload/v1594425842/Placeholder_2_fl0psx.png',
+      src: 'v1594425842/Placeholder_2_fl0psx.png',
     },
     {
       index: 3,
       src:
-        'https://res.cloudinary.com/favourcodes/image/upload/v1594510804/joao-paulo-de-souza-oliveira-BBNPJnucqYQ-unsplash_d5ns7s.jpg',
+        'v1594510804/joao-paulo-de-souza-oliveira-BBNPJnucqYQ-unsplash_d5ns7s.jpg',
     },
     {
       index: 4,
       src:
-        'https://res.cloudinary.com/favourcodes/image/upload/v1594510806/honey-yanibel-minaya-cruz-YrYdmIVSE8Y-unsplash_vxwda9.jpg',
+        'v1594510806/honey-yanibel-minaya-cruz-YrYdmIVSE8Y-unsplash_vxwda9.jpg',
     },
     {
       index: 5,
-      src:
-        'https://res.cloudinary.com/favourcodes/image/upload/v1594510808/juli-kosolapova-h6e1QApgI38-unsplash_ggey3g.jpg',
+      src: 'v1594510808/juli-kosolapova-h6e1QApgI38-unsplash_ggey3g.jpg',
     },
   ];
+
+  const onClick = useCallback(
+    item => setIndex([item.index - 1, item.index % images.length]),
+    [images.length]
+  );
+
+  const imageTransitions = useTransition(images[index], item => item.index, {
+    from: {
+      opacity: 0,
+      transform: 'translateX(150px)',
+    },
+    enter: {
+      opacity: 1,
+      transform: 'translateX(0%)',
+    },
+    leave: {
+      opacity: 0,
+      transform: 'translateX(-100px)',
+    },
+    config: config.molasses,
+  });
 
   return (
     <main>
@@ -57,33 +75,39 @@ const Hero = () => {
         </div>
         <div className='index'>
           {images.map(item => (
-            <animated.div
-              onClick={() => setIndex(item.index - 1)}
+            <div
+              key={item.index}
+              onClick={() => onClick(item)}
               className={
                 index === item.index - 1 ? 'numbers active' : 'numbers'
               }
             >
               {item.index}
-            </animated.div>
+            </div>
           ))}
         </div>
       </div>
 
       <div className='hero-image'>
         <figure>
-          <img
-            onMouseEnter={() => setActive(!active)}
-            onMouseLeave={() => setActive(!active)}
-            className='image'
-            alt=''
-            src={images[index].src}
-          />
+          {imageTransitions.map(({ item, props, key }) => (
+            <animated.div
+              className='image'
+              key={key}
+              onMouseEnter={() => setActive(!active)}
+              onMouseLeave={() => setActive(!active)}
+              style={{
+                ...props,
+                backgroundImage: `url(https://res.cloudinary.com/favourcodes/image/upload/${item.src})`,
+              }}
+            />
+          ))}
         </figure>
         <div className={active ? 'preview' : 'next'}>
           <img
             className='next-image'
             alt=''
-            src={images[(index + 1) % images.length].src}
+            src={`https://res.cloudinary.com/favourcodes/image/upload/${images[next].src}`}
           />
         </div>
       </div>
