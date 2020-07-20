11cc656d7fb830eda99008c6ba3c80dae36f10c7
lilpolymath <ayofavour7@gmail.com>
2020-07-20 12:29:33 +0100

    chore: App.js comment cleanup

--- src/App.js
+++ src/App.js
@@ -14,68 +14,29 @@ import HeroSeven from './screens/Seven';
 function App() {
   return (
     <Router>
-      <div>
-        <Switch>
-          <Route
-            exact
-            path='/'
-            // render={() => (
-            //   <ul>
-            // <li>
-            //   <Link style={{ color: 'black' }} to='/one'>
-            //     One
-            //   </Link>
-            // </li>
-            //     <li>
-            //       <Link style={{ color: 'black' }} to='/two'>
-            //         Two
-            //       </Link>
-            //     </li>
-            //     <li>
-            //       <Link style={{ color: 'black' }} to='/three'>
-            //         Three
-            //       </Link>
-            //     </li>
-            //     <li>
-            //       <Link style={{ color: 'black' }} to='/four'>
-            //         Four
-            //       </Link>
-            //     </li>
-            //     <li>
-            //       <Link style={{ color: 'black' }} to='/five'>
-            //         Five
-            //       </Link>
-            //     </li>
-            //     <li>
-            //       <Link style={{ color: 'black' }} to='/seven'>
-            //         Seven
-            //       </Link>
-            //     </li>
-            //   </ul>
-            // )}
-          >
-            <Landing />
-          </Route>
-          <Route path='/one'>
-            <HeroOne />
-          </Route>
-          <Route path='/two'>
-            <HeroTwo />
-          </Route>
-          <Route path='/three'>
-            <HeroThree />
-          </Route>
-          <Route path='/four'>
-            <HeroFour />
-          </Route>
-          <Route path='/five'>
-            <HeroFive />
-          </Route>
-          <Route path='/seven'>
-            <HeroSeven />
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact path='/'>
+          <Landing />
+        </Route>
+        <Route path='/one'>
+          <HeroOne />
+        </Route>
+        <Route path='/two'>
+          <HeroTwo />
+        </Route>
+        <Route path='/three'>
+          <HeroThree />
+        </Route>
+        <Route path='/four'>
+          <HeroFour />
+        </Route>
+        <Route path='/five'>
+          <HeroFive />
+        </Route>
+        <Route path='/seven'>
+          <HeroSeven />
+        </Route>
+      </Switch>
     </Router>
   );
 }
