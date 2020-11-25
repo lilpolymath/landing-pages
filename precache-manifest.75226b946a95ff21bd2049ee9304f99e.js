086dec59f6b574b7180df0aa07ed229a6204bf36
lilpolymath <ayofavour7@gmail.com>
2020-11-25 06:47:21 +0100

    Updates

---
+++ precache-manifest.75226b946a95ff21bd2049ee9304f99e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5341700c2c84286431ae0434d5a97157",
+    "url": "/landing-pages/index.html"
+  },
+  {
+    "revision": "e459cddda5c6321af9ca",
+    "url": "/landing-pages/static/css/main.2c5e9808.chunk.css"
+  },
+  {
+    "revision": "0d60f5bbcbc52d77987c",
+    "url": "/landing-pages/static/js/2.1f13896e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/landing-pages/static/js/2.1f13896e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e459cddda5c6321af9ca",
+    "url": "/landing-pages/static/js/main.65b6dfdc.chunk.js"
+  },
+  {
+    "revision": "a3437cbf0bc14f37f26c",
+    "url": "/landing-pages/static/js/runtime-main.fafd88ae.js"
+  },
+  {
+    "revision": "38e4d805ee3dba47f9f83b7db5eda138",
+    "url": "/landing-pages/static/media/A.38e4d805.svg"
+  },
+  {
+    "revision": "b7f7701bef9b1ef7ef99f44c491b87d3",
+    "url": "/landing-pages/static/media/Double.b7f7701b.svg"
+  },
+  {
+    "revision": "46fb4d147e2b938a09b2840794a3b8b2",
+    "url": "/landing-pages/static/media/G.46fb4d14.svg"
+  },
+  {
+    "revision": "bded0a4d84a85403284bc38d5bdca6bf",
+    "url": "/landing-pages/static/media/Single.bded0a4d.svg"
+  }
+]);
